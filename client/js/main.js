a9975aba6d4af0f595c4d2baa033a13467f22ad9
WouterK12 <wouterkoopman1@gmail.com>
2021-09-19 22:51:50 +0200

    Added users always showing in list

--- client/js/main.js
+++ client/js/main.js
@@ -6,10 +6,20 @@ var timespan = localStorage.getItem(CONSTANT.IDENTIFIER);
 if (!timespan) timespan = CONSTANT.TIMESPAN.FOURHOURS;
 
 async function Refresh() {
-  const users = await FetchData("/api/users");
-  UpdateGraph(users);
-  const statistics = await FetchData("/api/statistics");
-  UpdateStatistics(statistics);
+  var updatedText = document.getElementById("updated");
+
+  try {
+    const users = await FetchData("/api/users");
+
+    UpdateGraph(users);
+    const statistics = await FetchData("/api/statistics");
+    UpdateStatistics(statistics);
+
+    const updatedDate = new Date();
+    updatedText.innerText = `Updated: ${updatedDate.toLocaleString()}`;
+  } catch (err) {
+    updatedText.innerText = `Whoops! Something went wrong!\n(${err})`;
+  }
 }
 
 async function FetchData(endpoint) {
@@ -39,10 +49,15 @@ function UpdateGraph(data) {
   dataTable.addColumn({ type: "date", id: "Start" });
   dataTable.addColumn({ type: "date", id: "End" });
 
+  // get begin and end dates that belong to selected time period
   var startDates = GetScalingDates(timespan);
 
   // parse data from API and add to table
   var parsedData = ParseData(data, startDates[0]);
+
+  // update users list
+  UpdateUsers(data, parsedData);
+
   if (!parsedData || !parsedData.length) {
     container.innerText = "No data was found for this time period.";
     return;
@@ -58,7 +73,7 @@ function UpdateGraph(data) {
     },
     timeline: {
       showBarLabels: false,
-      rowLabelStyle: { fontName: "Patua One", fontSize: 24, color: "#000" },
+      rowLabelStyle: { fontName: "Patua One", fontSize: 18, color: "#000" },
     },
   };
 
@@ -69,8 +84,8 @@ function UpdateGraph(data) {
     options.hAxis.format = "dd/MM/yyyy";
   }
 
-  // make timestamp texts color white
   google.visualization.events.addListener(chart, "ready", function () {
+    // make timestamp texts color white
     var labels = container.getElementsByTagName("text");
     Array.prototype.forEach.call(labels, (label) => {
       if (label.getAttribute("text-anchor") === "middle") {
@@ -83,18 +98,15 @@ function UpdateGraph(data) {
 
   // draw chart
   chart.draw(dataTable, options);
-
-  // update users list
-  UpdateUsers(data, parsedData);
 }
 
 function UpdateUsers(data, parsedData) {
-  if (!data || !data.length) return;
-  if (!parsedData || !parsedData.length) return;
-
   const userList = document.getElementById("users");
   userList.innerHTML = null;
 
+  if (!data || !data.length) return;
+
+  // for every user
   for (let i = 0; i < data.length; i++) {
     const user = data[i];
 
@@ -103,9 +115,8 @@ function UpdateUsers(data, parsedData) {
     for (let i = 0; i < parsedData.length; i++) {
       if (parsedData[i][0] === user.name) latestEvent = parsedData[i];
     }
-    if (!latestEvent) continue;
 
-    // main div
+    // create main div
     let newUser = document.createElement("DIV");
     newUser.classList.add("user");
 
@@ -133,21 +144,24 @@ function UpdateUsers(data, parsedData) {
     let status = document.createElement("SPAN");
     status.classList.add("status");
 
-    // compare if date of latest event is close to now (within 10000ms)
-    const timeThreshold = 1e4;
-    var latestTime = latestEvent[4].getTime();
-    var nowTime = Date.now();
-    latestTime = Math.floor(latestTime / timeThreshold);
-    nowTime = Math.floor(nowTime / timeThreshold);
+    let connectionString = CONSTANT.PRESENCE.DISCONNECTED;
+    if (latestEvent) {
+      // compare if date of latest event is close to now (within 10000ms)
+      const timeThreshold = 1e4;
+      var latestTime = latestEvent[4].getTime();
+      var nowTime = Date.now();
+      latestTime = Math.floor(latestTime / timeThreshold);
+      nowTime = Math.floor(nowTime / timeThreshold);
 
-    const connectionString =
-      latestTime == nowTime
-        ? CONSTANT.PRESENCE.CONNECTED
-        : CONSTANT.PRESENCE.DISCONNECTED;
+      if (latestTime == nowTime) {
+        connectionString = CONSTANT.PRESENCE.CONNECTED;
+      }
+    }
     status.classList.add(connectionString);
 
     connection.appendChild(status);
 
+    // only show presence when connected
     if (connectionString === CONSTANT.PRESENCE.CONNECTED) {
       // connection time
       let time = document.createElement("SPAN");
